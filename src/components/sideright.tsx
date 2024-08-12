5bea23fc73bfab7c78472ed6ee961f52d13860f1
Wei <99aryaprima@gmail.com>
2024-08-12 11:05:43 +0700

    feat: sideright.tsx - switch account

--- src/components/sideright.tsx
+++ src/components/sideright.tsx
@@ -1,9 +1,24 @@
 const Sideright = () => {
   return (
-    <aside className="ps-16 py-8 max-xl:py-4">
+    <aside className="ps-16 py-8 max-xl:py-4 w-full max-w-[319px]">
       <div className="flex flex-col">
         {/* switch account */}
-        <div></div>
+        <div className="flex flex-row items-center justify-between w-full">
+          <div className="flex flex-row items-center gap-x-3">
+            <img
+              src="/profile.jpg"
+              alt="profile"
+              width={44}
+              height={44}
+              className="w-[44px] h-[44px] object-cover rounded-full"
+            />
+            <h6 className="text-xs font-bold tracking-tighter">433</h6>
+          </div>
+
+          <button type="button" className="font-normal text-[#0095f6] text-xs">
+            Switch
+          </button>
+        </div>
 
         {/* Suggested for you */}
         <div></div>
