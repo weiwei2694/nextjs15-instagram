af5482fab8ea8e087c59710b2281ad2a2653d0c4
Wei <99aryaprima@gmail.com>
2024-08-12 11:14:56 +0700

    feat: sideright.tsx - suggested for you

--- src/components/sideright.tsx
+++ src/components/sideright.tsx
@@ -1,7 +1,9 @@
+import { profiles } from "@/data";
+
 const Sideright = () => {
   return (
     <aside className="ps-16 py-8 max-xl:py-4 w-full max-w-[319px]">
-      <div className="flex flex-col">
+      <div className="flex flex-col space-y-6">
         {/* switch account */}
         <div className="flex flex-row items-center justify-between w-full">
           <div className="flex flex-row items-center gap-x-3">
@@ -15,13 +17,57 @@ const Sideright = () => {
             <h6 className="text-xs font-bold tracking-tighter">433</h6>
           </div>
 
-          <button type="button" className="font-normal text-[#0095f6] text-xs">
+          <button
+            type="button"
+            className="font-normal text-[#0095f6] text-xs hover:text-[f5f5f5] transition duration-75"
+          >
             Switch
           </button>
         </div>
 
         {/* Suggested for you */}
-        <div></div>
+        <div className="flex flex-col space-y-4">
+          <div className="flex flex-row justify-between items-center">
+            <h5 className="text-sm text-[#ababab] font-semibold">
+              Suggested for you
+            </h5>
+            <button
+              type="button"
+              className="text-xs text-[#f5f5f5] tracking-tight font-normal hover:text-[#ababab] duration-75 transition"
+            >
+              See all
+            </button>
+          </div>
+
+          {profiles.map((_, profileIndex) => {
+            if (profileIndex >= 5) return null;
+
+            return (
+              <div
+                key={profileIndex}
+                className="flex flex-row justify-between items-center"
+              >
+                <div className="flex flex-row items-center gap-x-3">
+                  <img
+                    src="/profile.jpg"
+                    alt="profile"
+                    width={44}
+                    height={44}
+                    className="w-[44px] h-[44px] object-cover rounded-full"
+                  />
+                  <h6 className="text-xs font-bold tracking-tighter">433</h6>
+                </div>
+
+                <button
+                  type="button"
+                  className="font-normal text-[#0095f6] text-xs hover:text-[f5f5f5] transition duration-75"
+                >
+                  Follow
+                </button>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </aside>
   );
