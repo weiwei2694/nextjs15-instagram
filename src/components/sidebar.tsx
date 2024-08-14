5d0665a58a76976e3fa38d1d5eedc80afe72c2aa
Wei <99aryaprima@gmail.com>
2024-08-14 13:53:12 +0700

    feat: sidebar.tsx - using reusable component display-icon.tsx

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,8 +1,8 @@
-import { sidebarLists, TSidebarList } from "@/data";
+import { sidebarLists } from "@/data";
 import { FaThreads } from "react-icons/fa6";
 import { Instagram, Menu } from "lucide-react";
-import { IoMdHome } from "react-icons/io";
 import Badge from "./ui/badge";
+import DisplayIcon from "./ui/display-icon";
 
 const Sidebar = () => {
   return (
@@ -63,28 +63,5 @@ const Sidebar = () => {
     </aside>
   );
 };
-
-const DisplayIcon = ({ list }: { list: TSidebarList }) => (
-  <div className="relative">
-    {/* BADGE */}
-    {list.title === "Messages" ? <Badge total={123} /> : null}
-
-    {list.title === "Home" ? (
-      <IoMdHome className="transition duration-75 xl:-translate-x-[2.5px] xl:w-[26.5px] xl:h-[26.5px] max-xl:w-[30.5px] max-xl:h-[30.5px] max-xl:group-hover:scale-105" />
-    ) : (
-      list.icon && (
-        <list.icon className="transition duration-75 xl:w-[22px] xl:h-[22px] max-xl:w-[24px] max-xl:h-[24px] max-xl:group-hover:scale-105" />
-      )
-    )}
-
-    {list.img && (
-      <img
-        src={list.img}
-        alt="profile"
-        className="object-cover rounded-full xl:w-[22px] xl:h-[22px] max-xl:w-[24px] max-xl:h-[24px] max-xl:rounded-full max-xl:group-hover:scale-105 max-xl:transition max-xl:duration-75"
-      />
-    )}
-  </div>
-);
 
 export default Sidebar;
