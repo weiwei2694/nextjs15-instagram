0dfac311b6d45f08af664422efd98e088a086388
Wei <99aryaprima@gmail.com>
2024-08-15 11:10:25 +0700

    feat: create-post.tsx - desktop version

--- src/components/ui/create-post.tsx
+++ src/components/ui/create-post.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Dialog,
   DialogContent,
@@ -6,19 +8,59 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { FileVideo, Image } from "lucide-react";
 import React from "react";
 
 const CreatePost = ({ children }: { children: React.ReactNode }) => {
+  const fileInputRef = React.useRef<null | HTMLInputElement>(null);
+
+  const handleButtonClick = () => {
+    if (!fileInputRef?.current) return null;
+    fileInputRef.current.click();
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
-      <DialogContent>
+      <DialogContent className="min-w-[348px] min-h-[391px] max-w-[855px] max-height-[898px] p-0 bg-[#262626]">
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </DialogDescription>
+          <div className="h-[43px] border-b border-gray-200/20 grid place-items-center">
+            <h4 className="text-[#f5f5f5] text-base font-semibold">
+              Create new post
+            </h4>
+          </div>
+
+          <div className="h-[734px] flex items-center justify-center">
+            <div className="flex flex-col items-center gap-y-6">
+              <div className="relative">
+                <Image className="w-16 h-16 -rotate-6 -translate-x-4 translate-y-10" />
+                <FileVideo className="w-16 h-16 rotate-6 translate-x-4 bg-[#262626]" />
+              </div>
+
+              <h3 className="text-[#f5f5f5] text-xl">
+                Drag photos and videos here
+              </h3>
+
+              <form>
+                <div>
+                  <input
+                    type="file"
+                    ref={fileInputRef}
+                    accept="image/*,video/*"
+                    hidden
+                  />
+
+                  <button
+                    type="button"
+                    onClick={handleButtonClick}
+                    className="bg-[#0095f6] text-white py-[7px] px-4 rounded-lg text-sm"
+                  >
+                    Select from computer
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </DialogHeader>
       </DialogContent>
     </Dialog>
